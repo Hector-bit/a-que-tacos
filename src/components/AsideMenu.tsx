676c5080f9d17a088525381d8d304dbf4785abb0
Hector-bit <hectormartinez.2978@gmail.com>
2025-01-29 21:23:22 -0800

    removed framer-motion stuff

--- src/components/AsideMenu.tsx
+++ src/components/AsideMenu.tsx
@@ -1,44 +1,7 @@
-"use client"
+'use client'
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
-import { motion, useAnimationControls, Variants } from "framer-motion";
-
-const AsideMenuVariants: Variants = {
-  'closed': {
-    x: '100%',
-    transition: {
-      ease: 'easeOut',
-      duration: 0.3
-    }
-  },
-  'open': {
-    x: '0%',
-    transition: {
-      ease: 'easeOut',
-      duration: 0.3
-    }
-  }
-}
-
-const BlurBgVariants: Variants = {
-  'closed': {
-    x: '100%',
-    opacity: 0,
-    transition: {
-      ease: 'easeOut',
-      duration: 0.1
-    }
-  },
-  'open': {
-    x: [0, 0],
-    opacity: [0, 1],
-    transition: {
-      ease: 'easeOut',
-      duration: 0.1
-    }
-  }
-}
 
 interface AsideMenuProps {
   // isOpen: boolean
@@ -46,43 +9,29 @@ interface AsideMenuProps {
 
 const AsideMenu = ({  }:AsideMenuProps) => {
   const [isOpen, setIsOpen] = useState<Boolean>(false)
-  const AsideMenuControls = useAnimationControls();
-
-  useEffect(() => {
-    if(isOpen){
-      AsideMenuControls.start('open')
-    } else {
-      AsideMenuControls.start('closed')
-    }
-  },[isOpen, AsideMenuControls])
 
   return (
     <>
-      <motion.div 
-        className="fixed backdrop-blur-sm z-[2] w-full h-full"
-        initial={'closed'}
-        variants={BlurBgVariants} 
-        animate={AsideMenuControls} 
+      <div 
+        className={`fixed duration-150  ${isOpen?'z-[2] backdrop-blur-sm':'backdrop-blur-none z-[-1]'} w-full h-full`}
       />
-      <div className="bg-primary top-0 flex flex-row justify-between items-center p-3 sm:p-6 w-full">
+      <div className="z-[1] bg-primary top-0 flex flex-row justify-between items-center p-3 sm:p-6 w-full">
         <div className="">
           <Image className="max-h-12 sm:max-h-28 w-auto" src='/assets/flag.png' alt="mexico flag" width={999} height={999}/>
         </div>
         <div className="text-black text-xl sm:text-5xl uppercase font-bold">a que tacos</div>
-        <div id="nav-icon3" className={`z-[4] ${isOpen?'open':''}`} onClick={() => setIsOpen(!isOpen)}>
+        <div id="nav-icon3" className={`${isOpen?'open':''}`} onClick={() => setIsOpen(!isOpen)}>
           <span></span>
           <span></span>
           <span></span>
           <span></span>
         </div>
       </div>
+
       {/* ASIDE MENU */}
-      <motion.div
+      <div
         id='_menuOverlay'
-        initial={'closed'}
-        className="z-[3] w-full h-full flex flex-row justify-end inset-0 fixed"
-        variants={AsideMenuVariants}
-        animate={AsideMenuControls}
+        className={`z-[3] w-full h-full flex flex-row justify-end inset-0 fixed duration-300 ${isOpen?'translate-x-0':'translate-x-full'}`}
         onClick={() => setIsOpen(false)}
       >
         <div id='_menuContainer' className="flex flex-col items-center top-0 right-0 bg-primary w-[50%] max-w-[90%] p-4 sm:p-8">
@@ -94,7 +43,7 @@ const AsideMenu = ({  }:AsideMenuProps) => {
             <Link className="hover:underline " href={"/menu"}>Menu</Link>
           </div>
         </div>
-      </motion.div>
+      </div>
     </>
   )
 }
