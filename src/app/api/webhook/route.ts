39fad5bd5d860ad2d6364e73a43e368e63a5f1b7
Hector-bit <hectormartinez.2978@gmail.com>
2025-03-01 21:56:12 -0800

    reorganized code and added 20sec, hopefully netlify doesnt mind now that im giving it a response 200 before starting the delay

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -14,33 +14,80 @@ function delay(ms: number) {
 const getTimeFromSig = (str: string):{timeStamp: string, signature: string } => {
   const sliced = str.slice(2)
   const spliced = sliced.split(',')
-  // console.debug('TIMESTAMP: ', spliced[0], spliced)
   return { timeStamp: spliced[0], signature: spliced[1].slice(3)}
 }
 
-export async function POST(req: NextRequest) {
+const getOrderId = async(requestUrl: string) => {
+  let fetchOrderId = await axios.get(
+    requestUrl,
+    {
+      headers: {
+        'Authorization': `Bearer ${hosted_token}`
+      }
+    }
+  ) .then((res) => {
+    console.debug('getting order id', res.data)
+    return res.data
+  })
+    .catch((err) => {
+      console.debug('error fetching order id', err)
+      return NextResponse.json({ error: `could not get order id`}, { status: 500 });
+  })
+
+  console.log('order id request data: ', fetchOrderId)
+
+  return fetchOrderId.data.order.id
+} 
+
+// REQUEST CLOVER MACHINE TO PRINT RECIEPT
+const requestPrint = async(orderId: string) => {
+  const printBody = {
+    "orderRef": {
+      "id": orderId
+    }
+  }
+
+  await axios.post(
+    `${clover_url}/v3/merchants/${merchant_id}/print_event`,
+    JSON.stringify(printBody),
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Clover-Merchant-ID': merchant_id, 
+        'Authorization': `Bearer ${hosted_token}`
+      }
+    }
+  ) .then((res) => {
+    console.debug('MADE IT TO THE END')
+    return NextResponse.json({ message: 'posted print request'}, {status: 200})
+  })
+    .catch((err) => {
+      console.debug('error printing', err)
+      return NextResponse.json({ error: `could not post print request`}, { status: 500 });
+  })
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
   console.debug('ROUTE IS RUNNING')
   try {
+    // Data from webhook 
     const body = await req.text();
-    console.debug('body ', body)
-    const signatureData = req.headers.get("clover-signature") || "";
-
     const parsedBody = JSON.parse(body)
-
+    const signatureData = req.headers.get("clover-signature") || "";
     const { timeStamp, signature } = getTimeFromSig(signatureData)
 
     const dateAndBody = `${timeStamp}.${body}`;
 
-    // ✅ Verify HMAC signature
     const expectedSignature = crypto.createHmac("sha256", WEBHOOK).update(dateAndBody).digest("hex");
-    console.debug('expected:', expectedSignature, '\n', 'recieved: ', signature)
+    // console.debug('expected:', expectedSignature, '\n', 'recieved: ', signature)
+
     if (signature !== expectedSignature) {
       console.debug('WRONG SIGNING KEY')
       return NextResponse.json({ error: "Invalid Signature" }, { status: 401 });
     }
 
-    // console.debug("Verified Webhook:", JSON.parse(body));
-
+    NextResponse.json({ message: "Processing in background" }, { status: 200 });
+    await delay(20000)
     // PAYMENT IS APPROVED GET TO PRINTING THE RECIEPT ON THE CLOVER MAHCINE
     if(parsedBody.type === 'PAYMENT' && parsedBody.status === 'APPROVED'){
       console.debug('parsed', parsedBody)
@@ -51,51 +98,12 @@ export async function POST(req: NextRequest) {
       const requestUrl = `${clover_url}/v3/merchants/${merchant_id}/payments/${parsedBody.id}`
       console.log('request url:', requestUrl)
 
-      // GETTING THE ORDER ID FIRST BY USING THE PAYMENT ID
-      let fetchOrderId = await axios.get(
-        requestUrl,
-        {
-          headers: {
-            'Authorization': `Bearer ${hosted_token}`
-          }
-        }
-      ) .then((res) => {
-        console.debug('getting order id', res.data)
-        return res.data
-      })
-        .catch((err) => {
-          console.debug('error fetching order id', err)
-          return NextResponse.json({ error: `could not get order id`}, { status: 500 });
-      })
+      let orderId = await getOrderId(requestUrl)
+      console.log('what is this', orderId)
 
-      console.log('what is this', fetchOrderId)
-
-
-      const printBody = {
-        "orderRef": {
-          "id": fetchOrderId.order.id
-        }
-      }
 
       // REQUEST CLOVER MACHINE TO PRINT RECIEPT
-      await axios.post(
-        `${clover_url}/v3/merchants/${merchant_id}/print_event`,
-        JSON.stringify(printBody),
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Clover-Merchant-ID': merchant_id, 
-            'Authorization': `Bearer ${hosted_token}`
-          }
-        }
-      ) .then((res) => {
-        console.debug('MADE IT TO THE END')
-        return NextResponse.json({ message: 'posted print request'}, {status: 200})
-      })
-        .catch((err) => {
-          console.debug('error printing', err)
-          return NextResponse.json({ error: `could not post print request: payment status${parsedBody.status}`}, { status: 500 });
-      })
+      requestPrint(orderId)
 
     }
 
