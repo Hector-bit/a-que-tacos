1c3ca1bbfcddd468483a18ee7cae3b2c0f2f62d5
Hector-bit <hectormartinez.2978@gmail.com>
2025-03-17 23:27:36 -0700

    commented out business hours route

--- src/app/api/clover/business-hours/route.ts
+++ src/app/api/clover/business-hours/route.ts
@@ -9,21 +9,21 @@ const clover_url = process.env.CLOVER_BASE_URL || ""
 const merchant_id = process.env.MERCHANT_ID || ""
 const hosted_token = process.env.API_KEY || ""
 
-export async function GET(req: NextRequest):Promise<NextResponse<any>> {
-  const requestUrl = `${clover_url}/v3/merchants/${merchant_id}/opening_hours`
-  console.debug('HOURS IS RUNNING: ', req)
+// export async function GET(req: NextRequest):Promise<NextResponse<any>> {
+//   const requestUrl = `${clover_url}/v3/merchants/${merchant_id}/opening_hours`
+//   console.debug('HOURS IS RUNNING: ', req)
 
-  try {
-    const newDate = new Date()
-    console.log('date from server: ', newDate)
-    const checkIfTrue = isBetween11And6(newDate)
-    console.log('is it true?/', checkIfTrue)
-    return NextResponse.json({ body: checkIfTrue })
-  }
-  catch(error){
-    console.error("error fetching business hours:", error);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-  }
-}
+//   try {
+//     const newDate = new Date()
+//     console.log('date from server: ', newDate)
+//     const checkIfTrue = isBetween11And6(newDate)
+//     console.log('is it true?/', checkIfTrue)
+//     return NextResponse.json({ body: checkIfTrue })
+//   }
+//   catch(error){
+//     console.error("error fetching business hours:", error);
+//     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+//   }
+// }
 
 
